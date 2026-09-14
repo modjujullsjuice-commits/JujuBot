3a8f8f7bb98ba75db67f24f3e860fa0b095c56e8
modjujullsjuice-commits <modjujullsjuice@gmail.com>
2026-09-15 03:24:52 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "JujuBot", // required by Discord API, not shown in the client
         state: "testing!!! mwehehe",     // this is what people actually see
         type: 2,               // Custom
       },
